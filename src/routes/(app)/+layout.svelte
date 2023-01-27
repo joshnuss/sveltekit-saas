350e73660c75c8df4a75ff5ec9a31063f2635cba
Josh Nussbaum <joshnuss@gmail.com>
2023-01-27 18:29:01 -0500

    setup

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,54 @@
+<script>
+  import '../../app.css'
+</script>
+
+<header>
+  <img src="/logo.svg" alt="logo">
+
+  <nav>
+    <a href="/dashboard">Home</a>
+    <a href="/billing">Billing</a>
+    <button>Sign out</button>
+  </nav>
+</header>
+
+<main>
+  <slot/>
+</main>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    background: #ccc;
+  }
+  header img {
+    height: 60px;
+  }
+
+  header nav {
+    margin: 1rem;
+    display: flex;
+    gap: 1rem;
+  }
+
+  header button {
+    background: none;
+    border: none;
+    padding: 0;
+    margin: 0;
+    display: block;
+  }
+
+  header :is(a, button) {
+    text-decoration: none;
+    color: #333;
+    font-size: 1rem;
+    align-self: center;
+  }
+
+  main {
+    padding: 2rem;
+  }
+
+</style>
---
+++ src/routes/(public)/+layout.svelte
@@ -0,0 +1,54 @@
+<script>
+  import '../../app.css'
+</script>
+
+<header>
+  <img src="/logo.svg" alt="logo">
+
+  <nav>
+    <a href="/">Home</a>
+    <a href="/pricing">Pricing</a>
+    <button>Sign in</button>
+  </nav>
+</header>
+
+<main>
+  <slot/>
+</main>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    background: #ccc;
+  }
+  header img {
+    height: 60px;
+  }
+
+  header nav {
+    margin: 1rem;
+    display: flex;
+    gap: 1rem;
+  }
+
+  header button {
+    background: none;
+    border: none;
+    padding: 0;
+    margin: 0;
+    display: block;
+  }
+
+  header :is(a, button) {
+    text-decoration: none;
+    color: #333;
+    font-size: 1rem;
+    align-self: center;
+  }
+
+  main {
+    padding: 2rem;
+  }
+
+</style>
