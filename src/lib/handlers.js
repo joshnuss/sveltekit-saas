32d9e75217eff64fdaa36ca91f424749a61d187b
Josh Nussbaum <joshnuss@gmail.com>
2023-02-28 16:14:18 -0500

    Fix member handler

--- src/lib/handlers.js
+++ src/lib/handlers.js
@@ -1,4 +1,4 @@
-import { redirect } from '@sveltejs/kit'
+import { error, redirect } from '@sveltejs/kit'
 
 export function authenticated(handler) {
 	return async (event) => {
@@ -16,8 +16,8 @@ export function member(handler) {
 	return async (event) => {
 		const session = await event.locals.getSession()
 
-		if (session.plan?.handle != 'enterprise') {
-			throw redirect(403, "Enterprise plan is required")
+		if (!session.plan) {
+			throw error(403, 'Plan is required')
 		}
 
 		return handler(event)
